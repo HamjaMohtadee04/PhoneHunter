446d9d2560ef4fd19b958fd2324a1c811725a0e7
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2024-11-16 12:32:09 +0600

    add modal for show deatil

--- js/phone.js
+++ js/phone.js
@@ -41,8 +41,8 @@ phones.forEach(phone=>{
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
     </div>
     `
@@ -90,5 +90,13 @@ const handleShowAll =() =>{
 
 }
 
+// handle showdetails
+const handleShowDetails = async(id)=>{
+    console.log('show details',id)
+    //load single phone data (individual)
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json();
+    console.log(data)
+}
 
 // loadPhone()
